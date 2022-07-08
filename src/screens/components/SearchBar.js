caa2f2bea0b636fe335d658d4839eeea5314e85d
Sungchul Park <sungchulpark@Sungchuls-MacBook-Air.local>
2022-07-08 13:42:39 -0400

    useState, text onChangeText setup

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,37 +1,43 @@
 import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
-import { Feather } from '@expo/vector-icons'; //this is how to import vector-icons lib
+import { Feather } from "@expo/vector-icons"; //this is how to import vector-icons lib
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
-        <Feather name="search" style={styles.iconStyle} /> 
-        {/* How to add vector-icons */}
-      <TextInput 
-        style={styles.inputStyle} 
-        placeholder='Search' />
+      <Feather name="search" style={styles.iconStyle} />
+      {/* How to add vector-icons */}
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        //same as onChange
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    backgroundStyle: {
-        marginTop: 10,
-        backgroundColor: '#E5E1E1',
-        height: 50,
-        borderRadius: 5,
-        marginHorizontal: 15,
-        flexDirection: 'row'
-    },
-    inputStyle: {
-        flex: 1,
-        fontSize: 18
-    },
-    iconStyle: {
-        fontSize: 35,
-        alignSelf: 'center', //to position Search icon to the center
-        marginHorizontal: 15
-    }
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#E5E1E1",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center", //to position Search icon to the center
+    marginHorizontal: 15,
+  },
 });
 
 export default SearchBar;
