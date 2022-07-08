fd3849556318cf78f52f4ea34a8339a9e71dc955
Sungchul Park <sungchulpark@Sungchuls-MacBook-Air.local>
2022-07-08 13:30:09 -0400

    Using vector-icons lib

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons'; //this is how to import vector-icons lib
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
+        <Feather name="search" size={30} /> 
+        {/* How to add vector-icons */}
       <Text>Search Bar</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15
+    }
+});
 
 export default SearchBar;
