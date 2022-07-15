77d7a494ee64b03ba14e1236155d6e6668c32cce
Sungchul Park <sungchulpark@Sungchuls-MacBook-Air.local>
2022-07-15 15:55:15 -0400

    FlatList rendering

--- src/screens/components/ResultsList.js
+++ src/screens/components/ResultsList.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ResultsList = ({ title, results }) => {
 	return (
 		<View>
 			<Text style={styles.title}>{title}</Text>
-			<Text>Results: {results.length}</Text>
+			<FlatList
+				horizontal //same as horizontal={true} //by default, it renders vertically
+				data={results}
+				keyExtractor={(result) => result.id} //it will map through whatever you pass in 'data' prop. 'results' in this case
+				renderItem={({ item }) => {
+					//it has two properties:index, item. basically item is an object of 'results'
+					return <Text>{item.name}</Text>;
+				}}
+			/>
 		</View>
 	);
 };
