342f68cb5ffea94edc694be921245cc2d5f178a0
Sungchul Park <sungchulpark@Sungchuls-MacBook-Air.local>
2022-07-15 15:29:26 -0400

    custom hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,44 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = (searchTerm) => {
 	const [term, setTerm] = useState("");
-	const [results, setResults] = useState([]);
-	const [errorMessage, setErrorMessage] = useState("");
-
-	const searchApi = async (searchTerm) => {
-		try {
-			//for Error handling
-			const response = await yelp.get("/search", {
-				params: {
-					limit: 50,
-					term: searchTerm,
-					location: "san jose",
-				},
-			});
-			setResults(response.data.businesses);
-		} catch (err) {
-			console.log(err);
-			setErrorMessage("Something went wrong"); //if err occurs, print this message
-		}
-	};
-
-	//Call searchApi when component is first rendered.
-	//BAD CODE! because it will fire this function multiple times.
-	// searchApi("pasta");
-
-	useEffect(() => {
-		searchApi("pasta"); //Run this once when this cmp is first rendered
-	}, []); //re run every time this changes.
+	const [searchApi, results, errorMessage] = useResults(); //these are going to come from calling useResults.
 
 	return (
 		<View>
 			<SearchBar
 				term={term}
 				onTermChange={setTerm}
-				onTermSubmit={() => searchApi(term)} //need an arrow to pass arg
+				onTermSubmit={() => searchApi(term)}
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>We have found {results.length}</Text>
